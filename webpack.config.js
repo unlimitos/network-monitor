cdc48d1af7446d28f13fe302da3e4b7e7f2868f5
tranquochuy <unlimitos@gmail.com>
2017-04-15 01:34:24 +0700

    Commit config dev enviroment

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
 var webpack = require('webpack');
 var path = require('path');
-
+var HtmlWebPackPlugin = require('html-webpack-plugin');
+const VENDOR_LIBS = [ "faker", "lodash","react","react-dom", "react-input-range", "react-redux", "react-router", "redux", "redux-form","redux-thunk"];
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+      bundle: './src/index.js',
+      vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: '[name].[chunkhash].js'
+  },
+  module : {
+    rules : [
+        {
+          use: 'babel-loader',
+          test:/\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          use :['style-loader','css-loader'],
+          test: /\.css$/
+        }
+    ]
+  },
+  plugins : [
+      new webpack.optimize.CommonsChunkPlugin({
+            name : ['vendor', 'manifest']
+      }),
+      new HtmlWebPackPlugin({
+            template: 'src/index.html'
+      })
+
+  ]
 };
