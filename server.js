c6d9ae58b387b84a8952ccd63363a5d26253945c
tranquochuy <unlimitos@gmail.com>
2017-04-15 12:04:59 +0700

    Finish startkit support Dev server and deploy server

---
+++ server.js
@@ -0,0 +1,21 @@
+/**
+ * Created by tranquochuy on 4/15/17.
+ */
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+if (process.env.NODE_ENV !== 'production'){
+    const webpackMiddleware = require('webpack-dev-middleware');
+    const webpack = require('webpack');
+    const webpackConfig=require('./webpack.config');
+    app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+    app.use(express.static('dist'));
+    app.get('*',(req,res)=>{
+        app.sendFile(path.join(__dirname,'dist/index.html'))
+    })
+}
+
+app.listen(process.env.PORT || 3050,()=>console.log("Listening on port : ",3050));
